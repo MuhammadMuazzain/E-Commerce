d99f9c50cd33f117d77d3b1c3a1876051071bf91
Muhammad Muazzain <muhammadmuazzain07@gmail.com>
2024-03-20 08:02:48 +0500

    second-commit

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory'
+import LoginSingup from './Pages/LoginSingup'
+import Cart from './Pages/Cart'
+import Product from './Pages/Product'
+import Shop from './Pages/Shop'
 
 const App = () => {
   return (
     <div>
+    <BrowserRouter>
       <Navbar/>
+      <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory/>} category="mens"/>
+          <Route path='/womens' element={<ShopCategory/>} category="women"/>
+          <Route path='/kids' element={<ShopCategory/>} category="kids"/>
+
+          <Route path='/product' element={<Product/>}/>
+          <Route path=':productId' element={<Product/>}/>
+
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSingup/>}/>
+      </Routes>
+        
+
+      </BrowserRouter>
     </div>
   )
 }
