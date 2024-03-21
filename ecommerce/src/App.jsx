8f68bf91c9e0d8f74b0a01d094ba5c5fd628cbd2
Muhammad Muazzain <muhammadmuazzain07@gmail.com>
2024-03-21 08:08:32 +0500

    commit34

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -6,6 +6,7 @@ import LoginSingup from './Pages/LoginSingup'
 import Cart from './Pages/Cart'
 import Product from './Pages/Product'
 import Shop from './Pages/Shop'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -24,6 +25,8 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSingup/>}/>
       </Routes>
+
+      <Footer/>
         
 
       </BrowserRouter>
